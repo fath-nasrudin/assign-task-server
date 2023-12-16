f8828033801d36efd903f5a83b0fb205cb447f55
fath-nasrudin <fath.nasrudin.i@gmail.com>
2023-12-16 20:15:15 +0700

    Add basic routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/api', routes);
+app.get('/', (req, res) => res.redirect('/api'));
 
 module.exports = app;
