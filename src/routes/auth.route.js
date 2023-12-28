60eef2aa217ba7a6f10623d5c05189e68513e019
fath-nasrudin <fath.nasrudin.i@gmail.com>
2023-12-28 16:15:24 +0700

    Add login limiter middleware

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth.controller');
+const loginLimiter = require('../middleware/loginLimiter');
 
 router.route('/login')
-  .post(authController.login);
+  .post(loginLimiter, authController.login);
 
 router.route('/refresh')
   .get(authController.refresh);
